db66cb28aefedad1af75e8c815bdb0023f922262
williamburger <burgerw@Williams-MacBook-Pro.local>
2017-05-25 23:07:40 -0400

    editor

--- db.py
+++ db.py
@@ -269,58 +269,50 @@ def registerAuthor(n, e, a,p,p2):
 if __name__ == "__main__":
     try:
 
-      HOST = "mongodb://Team23:Muv58mTtDaweFhrU@cluster0-shard-00-00-ppp7l.mongodb.net:27017,cluster0-shard-00-01-ppp7l.mongodb.net:27017,cluster0-shard-00-02-ppp7l.mongodb.net:27017/Team23DB?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
-      client = MongoClient(HOST);
-      db = client.Team23DB
-      collection = db.burger
-      collection.insert_one({ "text": "hello" })
+        HOST = "mongodb://Team23:Muv58mTtDaweFhrU@cluster0-shard-00-00-ppp7l.mongodb.net:27017,cluster0-shard-00-01-ppp7l.mongodb.net:27017,cluster0-shard-00-02-ppp7l.mongodb.net:27017/Team23DB?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
+        client = MongoClient(HOST);
+        db = client.Team23DB
+        collection = db.burger
+        collection.insert_one({ "text": "hello" })
 
-    except ValueError:
-      print("There was an error")
-
-      print("Connection established. Welcome.\n")
-      user_input = raw_input("First, please enter the master key for encrypting passwords: ")
-      MASTER_KEY = user_input
-
-      user_input = raw_input("If You're an Author, Enter 'Author' \n"
-                             "If You're an Editor, Enter 'Editor' \n"
-                             "If You're a Reviewer, Enter 'Reviewer'\n"
-                             "To Logout, Enter 'Logout'\n")
-      if (user_input == 'Author'):
-         user_input = raw_input("If you have previously signed up, login by typing in your unique id.\n"
-                "Otherwise, type 'Register'\n")
-         if (user_input == 'Register'):
-             registerAuthor(None, None, None,None, None)
-         else:
-             password = getpass.getpass("Please enter your password: ")
-             loginAuthor(user_input, password)
-      elif (user_input == 'Editor'):
-          user_input = raw_input("If you have previously signed up, login by typing in your unique id.\n"
-                 "Otherwise, type 'Register'\n")
-          if (user_input == 'Register'):
-                editor.registerEditor(None,None,None,MASTER_KEY,con)
-          else:
+        print("Connection established. Welcome.\n")
+        user_input = raw_input("If You're an Author, Enter 'Author' \n"
+                    "If You're an Editor, Enter 'Editor' \n"
+                    "If You're a Reviewer, Enter 'Reviewer'\n"
+                    "To Logout, Enter 'Logout'\n")
+        if (user_input == 'Author'):
+            user_input = raw_input("If you have previously signed up, login by typing in your unique id.\n"
+            "Otherwise, type 'Register'\n")
+            if (user_input == 'Register'):
+                registerAuthor(None, None, None,None, None)
+            else:
                 password = getpass.getpass("Please enter your password: ")
-                editor.loginEditor(user_input,password,MASTER_KEY,con)
-      elif (user_input == 'Reviewer'):
-          user_input = raw_input("If you have previously signed up, login by typing in your unique id.\n"
-                 "If You wish to resign, type 'RESIGN <id>'\n"
-                 "Otherwise, type 'Register'\n")
-          inputArr = user_input.split(' ')
-          if (user_input == 'Register'):
+                loginAuthor(user_input, password)
+        elif (user_input == 'Editor'):
+            user_input = raw_input("If you have previously signed up, login by typing in your unique id.\n"
+             "Otherwise, type 'Register'\n")
+            if (user_input == 'Register'):
+                editor.registerEditor(None,db)
+            else:
+                password = getpass.getpass("Please enter your password: ")
+                editor.loginEditor(user_input,db)
+        elif (user_input == 'Reviewer'):
+            user_input = raw_input("If you have previously signed up, login by typing in your unique id.\n"
+            "If You wish to resign, type 'RESIGN <id>'\n"
+            "Otherwise, type 'Register'\n")
+            inputArr = user_input.split(' ')
+            if (user_input == 'Register'):
                 reviewer.registerReviewer(None,None,None,None,None,MASTER_KEY,con)
-          elif(inputArr[0] =='RESIGN'):
+            elif(inputArr[0] =='RESIGN'):
                 reviewer.resign(inputArr[1],con)
-          else:
-                password = getpass.getpass("Please enter your password: ")
+            else:
                 reviewer.loginReviewer(user_input,password,MASTER_KEY,con)
-      elif (user_input == "Logout"):
-          print("Ta ta for now!\n")
-          con.close()
-      else:
-          print("Shutting down.\n")
-    except pymongo.errors.ServerSelectionTimeoutError as err:        # catch SQL errors
-     print("Connection Failure")
-     print(err)
+        elif (user_input == "Logout"):
+            print("Ta ta for now!\n")
+        else:
+            print("Shutting down.\n")
+    except pymongo.errors.ServerSelectionTimeoutError as err:        # catch errors
+        print("Connection Failure")
+        print(err)
     except:                                   # anything else
-     print("Unexpected error: {0}".format(sys.exc_info()[0]))
+        print("Unexpected error: {0}".format(sys.exc_info()[0]))
